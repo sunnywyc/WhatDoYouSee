21a64745d01922fba56073d56d91193da3c1c598
Sunny Wong <107906470+sunnywyc@users.noreply.github.com>
2022-08-27 03:00:38 +1000

    Adjust canvas width

--- sketch.js
+++ sketch.js
@@ -11,9 +11,9 @@ function preload() {
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(displayWidth, windowHeight);
   //initiating data for bubbbles in grid
-  nRow = int(windowHeight/50); nCol = (windowWidth/50);
+  nRow = int(windowHeight/50); nCol = (displayWidth/50);
   for (let j = 0; j < nRow; j++) {
 	myBubble[j] = []
     for (let i = 0; i < nCol; i++) {
@@ -25,13 +25,13 @@ function setup() {
   //initiating data for spotlight
   spotNum = 8;
    for (let m = 0; m < spotNum; m++) {
-    mySpotlight[m] = new Spotlight(random(windowWidth/2-50,windowWidth/2+50),random(windowHeight/2),200,random(-1,1),random(-1,1),createGraphics(windowWidth, windowHeight))
+    mySpotlight[m] = new Spotlight(random(displayWidth/2-50,displayWidth/2+50),random(windowHeight/2),200,random(-1,1),random(-1,1))
   }
 
-   //initiating data for answer
+   //initiating data for spotlight
   ansNum = 5;
    for (let h = 0; h < ansNum; h++) {
-  myAnswer[h] = new Answer((h+1)*(windowWidth-200)/5, random(windowHeight*0.6,windowHeight*0.9), color(227,189,88), h);
+  myAnswer[h] = new Answer((h+1)*(displayWidth-200)/5, random(windowHeight*0.6,windowHeight*0.9), color(227,189,88), h);
    }
   textFont(font);
   textAlign(CENTER,CENTER);
@@ -43,27 +43,26 @@ function draw() {
   textSize(24);
   fill(227,189,88);
   text(text01, windowWidth/2, windowHeight*0.25);
-	
+  
   //random keywords display
   for (let h = 0; h < myAnswer.length; h++) {
     myAnswer[h].display();
   }
   pop();
-	
+  
   for (let j = 0; j < myBubble.length; j++) {
     for (let i = 0; i < myBubble[j].length; i++) {
-      // print(myBubble[0]);   
       myBubble[j][i].jitter();//animate bubbles
       myBubble[j][i].display();//drawing bubbles
     }
   }
   
   for (let m = 0; m < mySpotlight.length; m++) {
-    // mySpotlight[m].canvas();
     mySpotlight[0].move();//animate spotlight
     mySpotlight[0].erasor();//drawing spotlight
+  
   }
-
+  
   push();
   fill(227,189,88);
   if(windowWidth<=809){
@@ -87,13 +86,13 @@ function draw() {
 }
 
 class Spotlight {
-  constructor(x, y, diameter, xSpeed, ySpeed,img) {
+  constructor(x, y, diameter, xSpeed, ySpeed) {
     this.x = x;
     this.y = y;
     this.diameter = diameter;
     this.xSpeed = xSpeed;
     this.ySpeed = ySpeed;
-    this.img = img;//creating another canvas on top
+    this.img = createGraphics(displayWidth,windowHeight);//creating another canvas on top
   }
   
   move(){
@@ -120,9 +119,8 @@ class Spotlight {
   //this.img.fill(0);
   this.img.ellipse(this.x,this.y,this.diameter);
   image(this.img, 0,0,width,height);//display the top canvas
-  }
+  }  
 }
-
 class Bubble {
   constructor(x, y, diameter, xSpeed, ySpeed, myColor) {
     this.x = x;
