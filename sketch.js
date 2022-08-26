896318907822fa8db482f0901da14e0c24c7ca59
Sunny Wong <107906470+sunnywyc@users.noreply.github.com>
2022-08-26 21:50:03 +1000

    Resizable text
    Added breakpoint to text.

--- sketch.js
+++ sketch.js
@@ -11,9 +11,9 @@ function preload() {
 
 
 function setup() {
-  createCanvas(windowWidth, 600);
+  createCanvas(windowWidth, windowHeight);
   //initiating data for bubbbles in grid
-  nRow = int(height/50); nCol = (width/50);
+  nRow = int(windowHeight/50); nCol = (windowWidth/50);
   for (let j = 0; j < nRow; j++) {
 	myBubble[j] = []
     for (let i = 0; i < nCol; i++) {
@@ -25,15 +25,14 @@ function setup() {
   //initiating data for spotlight
   spotNum = 8;
    for (let m = 0; m < spotNum; m++) {
-    mySpotlight[m] = new Spotlight(random(width/2-50,width/2+50),random(height/2),200,random(-1,1),random(-1,1))
+    mySpotlight[m] = new Spotlight(random(windowWidth/2-50,windowWidth/2+50),random(windowHeight/2),200,random(-1,1),random(-1,1),createGraphics(windowWidth, windowHeight))
   }
 
-   //initiating data for spotlight
+   //initiating data for answer
   ansNum = 5;
    for (let h = 0; h < ansNum; h++) {
-  myAnswer[h] = new Answer((h+1)*(width-200)/5, random(height*0.6,height*0.9), color(227,189,88), h);
+  myAnswer[h] = new Answer((h+1)*(windowWidth-200)/5, random(windowHeight*0.6,windowHeight*0.9), color(227,189,88), h);
    }
-  
   textFont(font);
   textAlign(CENTER,CENTER);
 }
@@ -43,12 +42,14 @@ function draw() {
   push();
   textSize(24);
   fill(227,189,88);
-  text(text01, width/2, height*0.25);
+  text(text01, windowWidth/2, windowHeight*0.25);
+	
   //random keywords display
   for (let h = 0; h < myAnswer.length; h++) {
     myAnswer[h].display();
   }
   pop();
+	
   for (let j = 0; j < myBubble.length; j++) {
     for (let i = 0; i < myBubble[j].length; i++) {
       // print(myBubble[0]);   
@@ -62,35 +63,51 @@ function draw() {
     mySpotlight[0].move();//animate spotlight
     mySpotlight[0].erasor();//drawing spotlight
   }
+
   push();
-  textSize(36);
   fill(227,189,88);
-  text("Playful Digital Experiences + Cultural Heritage", width/2, height/2);
+  if(windowWidth<=809){
+  textSize(16);
+  textWrap(WORD);
+  text("Playful Digital Experiences + Cultural Heritage", windowWidth/2, windowHeight/2);  
+  }
+  else if(windowWidth>809 && windowWidth<=1199){
+  textSize(28);
+  text("Playful Digital Experiences + Cultural Heritage", windowWidth/2, windowHeight/2);
+  }
+  else if(windowWidth>1199 && windowWidth<=1799){
+  textSize(36);
+  text("Playful Digital Experiences + Cultural Heritage", windowWidth/2, windowHeight/2);
+  }
+  else if(windowWidth>1799){
+  textSize(36);
+  text("Playful Digital Experiences + Cultural Heritage", windowWidth/2, windowHeight/2);
+  }
   pop();
 }
 
 class Spotlight {
-  constructor(x, y, diameter, xSpeed, ySpeed) {
+  constructor(x, y, diameter, xSpeed, ySpeed,img) {
     this.x = x;
     this.y = y;
     this.diameter = diameter;
     this.xSpeed = xSpeed;
     this.ySpeed = ySpeed;
-    this.img = createGraphics(windowWidth,600);//creating another canvas on top
+    this.img = img;//creating another canvas on top
   }
   
   move(){
-    if(mouseX>100 && mouseX<width-100 && mouseY>100 && mouseY<height-100){
+    if(mouseX>50 && mouseX<windowWidth-50 && mouseY>50 && mouseY<windowHeight-50){
       this.x = mouseX;
       this.y = mouseY;      
     }
     else{
       this.x += this.xSpeed*0.5;
-      if(this.x <= 0 || this.x >= width){
+      if(this.x <= 0 || this.x >= windowHeight){
         this.xSpeed *= -1;
       }
         this.y += this.ySpeed*0.5;
-      if(this.y <= 0 || this.y >= height){
+      if(this.y <= 0 || this.y >= windowHeight){
         this.ySpeed *= -1;
       }
     }
@@ -102,7 +119,7 @@ class Spotlight {
   this.img.erase();
   //this.img.fill(0);
   this.img.ellipse(this.x,this.y,this.diameter);
-  image(this.img, 0,0,windowWidth,600);//display the top canvas
+  image(this.img, 0,0,width,height);//display the top canvas
   }
 }
 
